ac0fcb2b25e49305f32984e64ee238bb2ba269ca
HieuDT <hieu@wiserobot.com>
2025-10-03 20:08:10 +0700

    fix pg

--- config/database.js
+++ config/database.js
@@ -1,4 +1,8 @@
 import Sequelize from 'sequelize'
+import pg from 'pg';
 // const { DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, DB_DIALECT } = process.env.DB_CONNECTION_STRING
-const sequelize = new Sequelize(process.env.DB_CONNECTION_STRING);
+const sequelize = new Sequelize(process.env.DB_CONNECTION_STRING, {
+  dialect: 'postgres',
+  dialectModule: pg
+});
 export default sequelize
