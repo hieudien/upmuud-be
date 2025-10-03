2ab5a3cd86d562e4e3bafca4e91da806c5a077bf
HieuDT <hieu@wiserobot.com>
2025-10-03 21:03:11 +0700

    try connect supabase

--- config/database.js
+++ config/database.js
@@ -3,6 +3,12 @@ import pg from 'pg';
 // const { DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, DB_DIALECT } = process.env.DB_CONNECTION_STRING
 const sequelize = new Sequelize(process.env.DB_CONNECTION_STRING, {
   dialect: 'postgres',
-  dialectModule: pg
+  dialectModule: pg,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 export default sequelize
